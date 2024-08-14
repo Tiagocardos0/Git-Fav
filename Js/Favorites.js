f9044db922d33ebc20089cbd61934f8a8eaeacc5
Tiago Cardoso <tiago.card2121@gmail.com>
2024-08-14 16:53:23 -0300

    ultimo updat
    Deixado a aplicação funcional, finalizado o JavaScript para pesquisas de usuario do GitHub. Aplicação funcionando.

--- Js/Favorites.js
+++ Js/Favorites.js
@@ -1,3 +1,18 @@
+export class GithubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`
+
+        return fetch(endpoint)
+        .then(data => data.json())    
+        .then(({ login, name, public_repos, followers}) => ({
+            login,
+            name,
+            public_repos,
+            followers
+        }))
+    }
+}
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
@@ -5,30 +20,44 @@ export class Favorites {
     }
 
     load() {
-        this.entries = [
-            {
-                login: 'Tiagocardos0',
-                name: 'Tiago cardoso',
-                public_repos: '24',
-                followers: '1000'
-            },
+        this.entries = JSON.parse(localStorage.getItem
+            ('@github-favorites')) || []
+    }
 
-            {
-                login: 'DFelipe1',
-                name: 'David Felipe',
-                public_repos: '32',
-                followers: '350'
+    save() {
+        localStorage.setItem('@github-favorites' , JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists) {
+                throw new Error('Este usuário ja existe')
             }
-        ]
+
+            const user = await GithubUser.search(username)
+             if (user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+             }
+
+             this.entries = [user, ...this.entries]
+             this.update()
+             this.save()
+
+        } catch(error) {
+            alert(error.message)
+        }
     }
 
     delete(user) {
         const FilteredEntries = this.entries.filter((entry)=> entry.login !== user.login)
-        console.log(FilteredEntries)
 
         this.entries = FilteredEntries
 
         this.update()
+        this.save()
     }
 }
 
@@ -39,6 +68,17 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onAdd()
+    }
+
+    onAdd() {
+        const BtnFavorite = this.root.querySelector('header .search button')
+
+        BtnFavorite.addEventListener('click', () => {
+            const {value} = this.root.querySelector('header .search input')
+
+            this.add(value)
+        })
     }
 
     update() {
@@ -49,6 +89,7 @@ export class FavoritesView extends Favorites {
 
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
@@ -78,8 +119,8 @@ export class FavoritesView extends Favorites {
 
         tr.innerHTML = `
             <td class="user">
-                <img src="https://github.com/Tiagocardos0.png" alt="">
-                <a href="https://github.com/Tiagocardos0" target="_blank"> <p></p> <span></span></a>
+                <img src="" alt="">
+                <a href="" target="_blank"> <p></p> <span></span></a>
                 
             </td>
             <td class="repositories">23</td>
